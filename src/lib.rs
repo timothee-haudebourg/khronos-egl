36d88bb45c8005d3424de971b0c10cce87dea157
Sean Kerr <sean@code-box.org>
2016-12-05 18:34:40 -0800

    updated documentation

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 // | See the License for the specific language governing permissions and                           |
 // | limitations under the License.                                                                |
 // +-----------------------------------------------------------------------------------------------+
-// | Author: Sean Kerr <sean@code-box.org>                                                         |
+// | Author: Sean Kerr <sean@metatomic.io>                                                         |
 // +-----------------------------------------------------------------------------------------------+
 
 #![crate_name = "egl"]
